1d34d96fdccd1ed6228be23c1a4cef1b295b5a2b
Mike Jones <mikejones@berkeley.edu>
2022-04-13 16:08:44 -0700

    change up the script

--- check.sh
+++ check.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
 
-get_layers() {
-    local repo=$1
-    local digest=$2
 
-    digestOutput=$(curl -H "Authorization: Bearer $(get_token $repo)" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" "https://index.docker.io/v2/${repo}/manifests/${digest}" 2>/dev/null)
+IFS=: read base_i base_t <<<$base
+IFS=: read image_i image_t <<<$image
 
-    jq -r '[.layers[].digest]' <<<"$digestOutput"
-}
+echo "getting docker token"
+token=$(curl 'https://auth.docker.io/token?service=registry.docker.io&scope=repository:'${base_i}':pull' 2>/dev/null | jq -r '.token')
+echo $token
 
-get_layers_ghcr() {
-    local repo=$1
-    local digest=$2
+echo "getting docker base layers"
+echo "curl -H 'Authorization: Bearer ${token}' -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' 'https://index.docker.io/v2/${base_i}/manifests/${base_t}'"
+docker_manifest=$(curl -H "Authorization: Bearer ${token}" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" "https://index.docker.io/v2/${base_i}/manifests/${base_t}" 2>/dev/null)
+echo "docker_manifest:"
+echo $docker_manifest
+base_layers=$(jq -r '[.layers[].digest]' <<<"$docker_manifest")
+echo "docker base layers:"
+echo $base_layers
 
-    digestOutput=$(curl -H "Authorization: Bearer $(echo $GITHUB_TOKEN | base64)" -H "Accept: application/vnd.docker.distribution.manifest.list.v2+json" https://ghcr.io/v2/${repo}/manifests/${digest} 2>/dev/null)
 
-    jq -r '[.layers[].digest]' <<<"$digestOutput"
-}
-get_token() {
-    local repo=$1
-    echo $(curl 'https://auth.docker.io/token?service=registry.docker.io&scope=repository:'${repo}':pull' 2>/dev/null | jq -r '.token')
-}
+echo "getting ghcr.io layers"
+ghcr_manifest=$(curl -H "Authorization: Bearer $(echo $GITHUB_TOKEN | base64)" -H "Accept: application/vnd.docker.distribution.manifest.list.v2+json" https://ghcr.io/v2/${image_i}/manifests/${image_t} 2>/dev/null)
+echo "ghcr manifest:"
+echo $ghcr_manifest
+images_layers=$(jq -r '[.layers[].digest]' <<<"$ghcr_manifest")
+echo "ghcr layers:"
+echo $images_layers
 
-IFS=: read base base_tag <<<$base
-IFS=: read image image_tag <<<$image
 
-layers_base=$(get_layers $base ${base_tag:-latest})
-layers_image=$(get_layers_ghcr $image ${image_tag:-latest})
 
-result=$(jq -cn "$layers_image - ($layers_image - $layers_base) | .!=[]")
+result=$(jq -cn "$images_layers - ($images_layers - $base_layers) | .!=[]")
 echo "::set-output name=result::${result}"
 
 
